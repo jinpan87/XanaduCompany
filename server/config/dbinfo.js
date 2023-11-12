9408d2ab1b9209b1765cbdce7897524978ac6428
robin <panj@jurassic.com.cn>
2023-11-12 21:55:14 +0800

    修改数据库链接配置

--- server/config/dbinfo.js
+++ server/config/dbinfo.js
@@ -1,8 +1,8 @@
 const config = {
-    database: "Xanadu", // 数据库名称
+    database: "xanadu-db", // 数据库名称
     username: "root", // 用户名
-    password: "12345678", // 密码
-    host: "localhost", // 主机地址
+    password: "password", // 密码
+    host: "192.168.159.132", // 主机地址
     port: "3306", // 端口号
     dialect: "mysql", //数据库类型，支持: 'mysql', 'sqlite', 'postgres', 'mssql'
     // logging: true, // 是否启用日志
