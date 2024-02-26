fe289d1f8627bc9bedcffbfcf5a6929b395d87c5
robin <panj@jurassic.com.cn>
2024-02-26 17:39:38 +0800

    本地配置

--- server/config/dbinfo.js
+++ server/config/dbinfo.js
@@ -1,7 +1,7 @@
 const config = {
     database: "xanadu-db", // 数据库名称
     username: "root", // 用户名
-    password: "root", // 密码
+    password: "password", // 密码
     host: "192.168.159.132", // 主机地址
     port: "3306", // 端口号
     dialect: "mysql", //数据库类型，支持: 'mysql', 'sqlite', 'postgres', 'mssql'
