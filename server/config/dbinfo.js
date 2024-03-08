b5ff39acef78e95fcb04d607c4eeb7635e89b6d4
robin <panj@jurassic.com.cn>
2024-03-08 10:56:18 +0800

    启用日志

--- server/config/dbinfo.js
+++ server/config/dbinfo.js
@@ -5,7 +5,7 @@ const config = {
     host: "192.168.159.128", // 主机地址
     port: "3306", // 端口号
     dialect: "mysql", //数据库类型，支持: 'mysql', 'sqlite', 'postgres', 'mssql'
-    // logging: true, // 是否启用日志
+    logging: true, // 是否启用日志
 }
 module.exports = config
 // sequelize-auto -h 'localhost' -d 'xanadu' -u 'root' -x '123456' -p 3307 -t 'aw_user'
