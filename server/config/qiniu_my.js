13ce0432ec17079a8c97e4e19c713e4a1a132c84
robin <panj@jurassic.com.cn>
2024-03-08 10:41:53 +0800

    更新配置

--- server/config/qiniu_my.js
+++ server/config/qiniu_my.js
@@ -1,13 +1,13 @@
 const config={
-    enable:false, //是否开启七牛
-    accessKey:'_FmIlvjEu1ZFo9tvImoA7XHtFjDpddxlKSn4KdiE',
-    secretKey:'wuzlv35tRQE98O_Dcd-aiATQh_fXbAyzLjm5gBWM',
-    bucket:'duanming', //存储桶名称
-    cdn:'image.zerotower.cn', //七牛配置的域名，七牛配置的测试域名只能使用一个月，记得配！
+    enable:true, //是否开启七牛
+    accessKey:'bmonQg5KKjATZo42GzI9WeOjpfQGGLRK6BMR7WYH',
+    secretKey:'aFZi2cyphBuAyVODE6R5cHkn1GRpWQrO0JqPg7O0',
+    bucket:'robin-2024', //存储桶名称
+    cdn:'sa099gfjq.hn-bkt.clouddn.com', //七牛配置的域名，七牛配置的测试域名只能使用一个月，记得配！
     zone:'z2',
-    rootPath:'company',//存储的根路径
+    rootPath:'image',//存储的根路径
     size:4, //最大文件大小，不得大于4M(单位M)
-    useHttps:true, //是否启用https
+    useHttps:false, //是否启用https
     useCdnDomains:false //是否启用cdn上传加速
 }
 
